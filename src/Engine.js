0cdd1964596e1b581224bb6e4e550d30d7288e66
fbomy <bomy.francois@gmail.com>
2016-10-11 13:37:22 +0200

    Première histoire ENGINE

--- src/Engine.js
+++ src/Engine.js
@@ -2,7 +2,20 @@
 
 var Engine = function () {
 
-// private attributes and methods
+var plateau;
 
-// public methods
+    this.init = function(){
+        this.plateau = [
+            ["Black", "Green", "White", "Blue", "Red", "White"],
+            ["Yellow", "White", "Green", "Red", "Yellow", "Blue"],
+            ["Blue", "Yellow", "Blue", "White", "Black", "Red"],
+            ["Red", "Black", "Red", "Green", "Blue", "White"],
+            ["White", "Green", "Yellow", "Black", "Yellow", "Green"],
+            ["Yellow", "Blue", "Black", "Red", "Green", "Black"]
+        ];
+        return this.plateau;
+    }
+    
+    
+
 };
